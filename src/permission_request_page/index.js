cce64be0b5faaab12391c01cd3d3b77d9a934a1f
NokiNyx <nokinyx@gmail.com>
2024-04-11 17:35:18 +0800

    Move back to manifest v2 for Firefox, and remove the on-install permission request.
    The on-install permission request was added when migrating to manifest v3,
    and is Firefox-specific.
    Now that we are migrating back to manifest v2 for Firefox, we no longer need this.

--- src/permission_request_page/index.js
+++
@@ -1,8 +0,0 @@
-const extension = browser ?? chrome;
-
-function requestPerms() {
-    extension.permissions.request({ origins: ['https://www.youtube.com/*']})
-        .then(granted => granted ? window.close() : null)
-}
-
-document.querySelector('button').addEventListener('click', requestPerms)
