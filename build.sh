54fca7392397965a0f5476be3301ce9e36ce4bf5
NokiNyx <nokinyx@gmail.com>
2024-05-05 18:29:52 +0800

    Clear build folder on every build.

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ declare -a BUILD_TARGETS=("firefox" "chromium")
 mkdir -p build
 
 for target in "${BUILD_TARGETS[@]}"; do
+    rm -r build/$target
     cp -R src -T build/$target
     BUILD_TARGET=$target pkl eval -f json manifest.pkl > build/$target/manifest.json
 done
